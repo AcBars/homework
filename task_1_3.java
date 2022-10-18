ff1390fcb4141836984964ec9756cb3bd9e65f6d
Mikhailov Artem <mobail_2003@bk.ru>
2022-10-18 22:39:22 +0700

    task_1_3 work v.3

--- task_1_3.java
+++ task_1_3.java
@@ -1,5 +1,35 @@
 // Реализовать простой калькулятор
 
+import java.util.Scanner;
 public class task_1_3 {
-    
+
+    public static int readInt() {
+        while (true) {
+            System.out.print("Введите число: ");
+            Scanner s = new Scanner(System.in);
+            String line = s.nextLine();
+            try {
+                return Integer.parseInt(line);
+            } catch (NumberFormatException e) {
+                System.err.println("Вы ввели не число!");
+            }
+        }
+    }
+    public static void main(String [] args) {
+        //System.out.print("Введите число: ");
+        // Scanner fl = new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
+        float num_1 = readInt();
+        System.out.print("Ведите знак действия: ");
+        String symbol = s.next();
+        //System.out.print("Введите число: ");
+        float num_2 = readInt();
+        int t = 2;
+        if(symbol.equalsIgnoreCase("+")){System.out.printf("%.2f+%.2f=%.2f", num_1, num_2, num_1+num_2);}
+        else if(symbol.equalsIgnoreCase("-")){System.out.printf("%.2f+%.2f=%.2f", num_1, num_2, num_1-num_2);}
+        else if(symbol.equalsIgnoreCase("*")){System.out.printf("%.2f+%.2f=%.2f", num_1, num_2, num_1*num_2);}
+        else if(symbol.equalsIgnoreCase("/")){System.out.printf("%.2f+%.2f=%.2f", num_1, num_2, num_1/num_2);}
+        else {System.out.print("Вы ввели не корректный знак действия введите + или - или * или /");}
+        
+    }
 }
--- task_1_4.java
+++ task_1_4.java
@@ -2,6 +2,23 @@
 например 2? + ?5 = 69. Требуется восстановить выражение до верного равенства. Предложить хотя бы одно
 решение или сообщить, что его нет. */
 
+import java.util.Scanner;
+
 public class task_1_4 {
-    
+    public static int readInt() {
+        while (true) {
+            System.out.print("Введите число: ");
+            Scanner s = new Scanner(System.in);
+            String line = s.nextLine();
+            try {
+                return Integer.parseInt(line);
+            } catch (NumberFormatException e) {
+                System.err.println("Вы ввели не число!");
+            }
+        }
+    }
+    public static void main(String [] args){
+        float t = readInt();
+        System.out.print(t);
+    }
 }
